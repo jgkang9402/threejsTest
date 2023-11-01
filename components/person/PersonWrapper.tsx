e1a013891298db6f902e83b08825e78e7cd41c15
jg <jgkang9402@gmail.com>
2023-11-02 00:19:38 +0900

    suspense test

--- components/person/PersonWrapper.tsx
+++ components/person/PersonWrapper.tsx
@@ -1,22 +1,30 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import React, { Suspense } from "react";
 import AnimaPerson from "./AnimaPerson";
 import Link from "next/link";
-
+const LoadingSpinner = () => {
+  return (
+    <>
+      <div className="spinner"></div>
+    </>
+  );
+};
 const PersonWrapper = () => {
   return (
     <>
       <Link href={"/"}>
         <button>홈으로</button>
       </Link>
-      <Canvas
-        style={{ width: "100vw", height: "100vh" }}
-        camera={{ near: 1, far: 100, position: [7, 7, 10] }}
-      >
-        <AnimaPerson />
-      </Canvas>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Canvas
+          style={{ width: "100vw", height: "100vh" }}
+          camera={{ near: 1, far: 100, position: [7, 7, 10] }}
+        >
+          <AnimaPerson />
+        </Canvas>
+      </Suspense>
     </>
   );
 };
